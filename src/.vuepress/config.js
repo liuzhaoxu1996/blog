8f3a1c9e8377d766b30a0bf6c1552bc28b35f2b8
liuzhaoxu <i-liuzhaoxu@360.cn>
2020-11-03 17:56:49 +0800

    feat: init

---
+++ src/.vuepress/config.js
@@ -0,0 +1,59 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Liuzhaoxu\'s blog',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: 'Lius的博客',
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  theme: 'reco',
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+    ],
+    sidebar: {
+      '/webpack/': [
+        {
+          title: 'webpack',
+          collapsable: false,
+          children: [
+            '',
+            'webpack5.x',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ src/index.md
@@ -0,0 +1,9 @@
+---
+home: true
+heroImage: https://v1.vuepress.vuejs.org/hero.png
+tagline:
+actionText: 快速开始 →
+actionLink: /webpack/
+
+footer: Copyright © 2020-present linus
+---
