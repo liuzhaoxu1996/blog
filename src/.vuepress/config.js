96e520de92899da388dbfb47c7ed51dcaaa3a952
liuzhaoxu <liuzhaoxu@kanzhun.com>
2023-11-29 18:38:29 +0800

    feat: update

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,8 +1,11 @@
-const fs = require('fs');
-const path = require('path');
-const blogList = fs.readdirSync(path.resolve(__dirname, '../blog')).map(item => {
-	return `/blog/${item.split('.')[0]}`
-}).reverse();
+const fs = require("fs");
+const path = require("path");
+const blogList = fs
+  .readdirSync(path.resolve(__dirname, "../blog"))
+  .map((item) => {
+    return `/blog/${item.split(".")[0]}`;
+  })
+  .reverse();
 module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
@@ -59,60 +62,60 @@ module.exports = {
         text: "博文",
         link: "/blog/1",
       },
-      //   {
-      //     text: "归档",
-      //     items: [
-      //       {
-      //         text: "go",
-      //         link: "/document/go/1",
-      //       },
-      //       {
-      //         text: "java",
-      //         link: "/document/java/1",
-      //       },
-      //       {
-      //         text: "flutter",
-      //         link: "/document/flutter/init",
-      //       },
-      //       {
-      //         text: "webpack5",
-      //         link: "/document/webpack/introduction",
-      //       },
-      //       {
-      //         text: "tensorflow",
-      //         link: "/document/tensorflow/1",
-      //       },
-      //       {
-      //         text: "mysql",
-      //         link: "/document/mysql/1",
-      //       },
-      //       {
-      //         text: "nodejs",
-      //         link: "/document/nodejs/1",
-      //       },
-      //       {
-      //         text: "面试",
-      //         link: "/document/interview/javascript",
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     text: "读书笔记",
-      //     items: [
-      //       {
-      //         text: "JavaScript函数式编程指南",
-      //         link: "/book/functional/first",
-      //       },
-      //       {
-      //         text: "JavaScript高级程序设计",
-      //         link: "/book/professional/1",
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     text: "算法",
-      //     link: "/algorithm/2",
-      //   },
+      {
+        text: "归档",
+        items: [
+          {
+            text: "go",
+            link: "/document/go/1",
+          },
+          {
+            text: "java",
+            link: "/document/java/1",
+          },
+          {
+            text: "flutter",
+            link: "/document/flutter/init",
+          },
+          {
+            text: "webpack5",
+            link: "/document/webpack/introduction",
+          },
+          {
+            text: "tensorflow",
+            link: "/document/tensorflow/1",
+          },
+          {
+            text: "mysql",
+            link: "/document/mysql/1",
+          },
+          {
+            text: "nodejs",
+            link: "/document/nodejs/1",
+          },
+          {
+            text: "面试",
+            link: "/document/interview/javascript",
+          },
+        ],
+      },
+      {
+        text: "读书笔记",
+        items: [
+          {
+            text: "JavaScript函数式编程指南",
+            link: "/book/functional/first",
+          },
+          {
+            text: "JavaScript高级程序设计",
+            link: "/book/professional/1",
+          },
+        ],
+      },
+      {
+        text: "算法",
+        link: "/algorithm/2",
+      },
     ],
     sidebarDepth: 1,
     sidebar: {
